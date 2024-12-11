3a0aa3afcc1798c35cc64c2b9100518481722f94
xiemuuu <monica629900@gmail.com>
2024-12-11 15:10:33 +0800

    Update SA.py

--- M2/SA.py
+++ M2/SA.py
@@ -1,4 +1,11 @@
-import random 
+import sys
+import os
+import uuid
+import random
+import time
+
+sys.path.append(os.path.abspath('../Dummy_Device_IoTtalk_v1_py-master'))  # 添加 LineNotify_folder 到模組搜尋路徑
+import Line  # 引入 LineNotify 模組 
 
 ServerURL = 'https://class.iottalk.tw' #For example: 'https://DomainName'
 MQTT_broker = 'iot.iottalk.tw' # MQTT Broker address, for example: 'DomainName' or None = no MQTT support
@@ -8,77 +15,108 @@ MQTT_User = 'iottalk'
 MQTT_PW = 'iottalk2023'
 
 device_model = 'M2'
-IDF_list = ['CO2', 'Luminance-I', 'Moisture', 'SoilTemp', 'pH']
+IDF_list = ['CO2', 'Moisture', 'SoilTemp', 'Luminance']
 # ODF_list = ['ODF313834004001','ODF313834004002']
 import uuid
 device_id = str(uuid.uuid4()) #if None, device_id = MAC address
-device_name = 'yc_M2'
+device_name = 'M2_final'
 exec_interval = 1  # IDF/ODF interval
 
 # def Dummy_Sensor():
 #     return random.randint(0, 100), random.randint(0, 100) 
 
 from sensors import sensorCO2_API
-from sensors import sensorEC_API
-from sensors import sensorPH_API
-from sensors import sensorUV_API
-# from sensors import sensorPS_API
-# from sensors import sensorSHT31_API
-
-ec = sensorEC_API()
-ec.start()
-
-ph = sensorPH_API()
-ph.start()
-
-uv = sensorUV_API()
-uv.start()
+# from sensors import sensorPH_API
+from sensors import sensorMT_API
+from sensors import sensorST_API
+from sensors import sensorLM_API
 
 co2 = sensorCO2_API()
 co2.start()
 
-# ps = sensorPS_API()
-# ps.start()
+# ph = sensorPH_API()
+# ph.start()
 
-# tp_and_rh = sensorSHT31_API()
-# tp_and_rh.start()
+mt = sensorMT_API()
+mt.start()
+
+st = sensorST_API()
+st.start()
+
+lm = sensorLM_API()
+lm.start()
 
 def CO2():
     c = co2.getData()
-    if c:  
-        print('co2', float(c))
+    if c is not None:
+        print(f"CO2 Sensor data: {c}")
         return float(c)
+    else:
+        print("CO2 Sensor failed to get data.")
+        return None
     
-def Luminance_I():
-    _, l, _ = uv.getData()
-    if l:  
-        print('luminance', float(l))
-        return float(l)
-    
+# def PH():
+#     p = ph.getData()
+#     if p is not None:
+#         print(f"PH Sensor data: {p}")
+#         return float(p)
+#     else:
+#         print("PH Sensor failed to get data.")
+#         return None
+
 def Moisture():
-    m, _, _ = ec.getData()
-    if m:  
-        print('moisture', float(m))
-        return float(m)
+    m = mt.getData()
+    if m is not None:
+        print(f"Moisture Sensor data: {m}")
+        return m
+    else:
+        print("Moisture Sensor failed to get data.")
+        return None
 
 def SoilTemp():
-    _, st, _ = ec.getData()
-    if st:  
-        print('soiltemp', float(st))
-        return float(st)
-
-def pH():
-    p = ph.getData()
-    if p:  
-        print('ph', float(p)) # no data
-        return float(p)
+    s = st.getData()
+    if s is not None:
+        print(f"Soil Temp Sensor data: {s}")
+        return s
+    else:
+        print("Soil Temp Sensor failed to get data.")
+        return None
     
+def Luminance():
+    l = lm.getData()
+    if l is not None:
+        print(f"Luminance Sensor data: {l}")
+        return l
+    else:
+        print("Luminance Sensor failed to get data.")
+        return None
+
+# 每次感測器數據更新後，將其發送到 LINE Notify
+def send_line_notify(message):
+    Line.notify(message)
+
 def on_register(r):
-    print(f'Device name: {r["d_name"]}')    
-    '''
-    #You can write some SA routine code here, for example: 
-    import time, DAI
+    print(f'Device name: {r["d_name"]}') 
+    
     while True:
-        DAI.push('Dummy_Sensor', [100, 200])  
-        time.sleep(exec_interval)    
-    '''
+        co2_data = CO2()
+        # ph_data = PH()
+        moisture_data = Moisture()
+        soil_temp_data = SoilTemp()
+        luminance_data = Luminance()
+
+        # 構建發送給 LINE Notify 的消息
+        message = f"CO2: {co2_data} ppm, Moisture: {moisture_data}, Soil Temp: {soil_temp_data} °C, Luminance: {luminance_data} lux"
+        
+        # 發送消息到 LINE Notify
+        send_line_notify(message)
+
+        # 等待下一次更新
+        time.sleep(exec_interval)  # 每隔一段時間抓取一次數據
+    # '''
+    # #You can write some SA routine code here, for example: 
+    # import time, DAI
+    # while True:
+    #     DAI.push('Dummy_Sensor', [100, 200])  
+    #     time.sleep(exec_interval)    
+    # '''
