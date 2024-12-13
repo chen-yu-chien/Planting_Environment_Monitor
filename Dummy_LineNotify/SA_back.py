a6a34021b5dfb939377f5eb4600777bd53ace488
陳俞蒨 <yuchien.ai13@nycu.edu.tw>
2024-12-13 17:55:20 +0800

    modify line notify file
    修改接收sensor input邏輯

--- Dummy_LineNotify/SA_back.py
+++ Dummy_LineNotify/SA_back.py
@@ -6,14 +6,21 @@ import importlib.util
 
 # 將 M2 資料夾加入 sys.path
 sys.path.append(os.path.abspath('../M2'))
+sys.path.append(os.path.abspath('../Dummy_AQISensor'))
 
 # 指定 M2/SA.py 的路徑
 M2_module_path = os.path.abspath('../M2/SA.py')
+AQI_module_path = os.path.abspath('../Dummy_AQISensor/SA.py')
 
 # 動態載入 M2/SA.py
 spec = importlib.util.spec_from_file_location("M2_SA", M2_module_path)
 M2_SA = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(M2_SA)
+
+# 動態載入 M2/SA.py
+spec = importlib.util.spec_from_file_location("AQI_SA", AQI_module_path)
+AQI_SA = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(AQI_SA)
 
 
 ServerURL = 'https://class.iottalk.tw' #For example: 'https://DomainName'
@@ -29,38 +36,19 @@ device_id = str(uuid.uuid4()) #if None, device_id = MAC address
 device_name = 'yc_Line'
 exec_interval = 10  # IDF/ODF interval
 
-co2, moisture, soil_temp, luminance, date, aqi = None, None, None, None, None, None
+date, aqi = "", ""
 
 import Line
 def Msg_O(data:list):
-    global co2, moisture, soil_temp, luminance, date, aqi
+    global date, aqi
     # Line.notify(data[0])
+    print(data[0], type(data[0]))
 
     if type(data[0]) is list:
-        match data[0][0]:
-            case "co2":
-                co2 = data[0][1]
-                print("co2:", co2)
-
-            case "luminance":
-                luminance = data[0][1]
-                print("luminance:", luminance)
-
-            case "moisture":
-                moisture = data[0][1]
-                print("moisture:", moisture)
-
-            case "soiltemp":
-                soil_temp = data[0][1]
-                print("soil_temp:", soil_temp)
-
-            case _:
-                date = data[0][0]
-                aqi = data[0][1]
-                print("date:", date, "aqi:", aqi)
-
-    else:
-        print("data[0] is not list")
+        if len(data[0]) > 1:
+            date = data[0][0]
+            aqi = data[0][1]
+            print(type(date), type(aqi))
 
 def on_register(r):
     print(f'Device name: {r["d_name"]}')    
@@ -73,24 +61,22 @@ def on_register(r):
     '''
 
     while True:
-        # co2_data = M2_SA.CO2()
-        # moisture_data = M2_SA.Moisture()
-        # soil_temp_data = M2_SA.SoilTemp()
-        # luminance_data = M2_SA.Luminance_I()
+        co2_data = M2_SA.CO2()
+        # ph_data = PH()
+        moisture_data = M2_SA.Moisture()
+        soil_temp_data = M2_SA.SoilTemp()
+        luminance_data = M2_SA.Luminance_I()
+        # date, aqi = AQI_SA.Dummy_Sensor()
 
-        print("Line co2:", co2)
-        print("Line moisture:", moisture)
-        print("Line soil_temp:", soil_temp)
-        print("Line luminance:", luminance)
         print("Line date:", date)
         print("Line aqi:", aqi)
 
         # 構建發送給 LINE Notify 的消息
-        m2_message = f"CO2: {co2} ppm, Moisture: {moisture}, Soil Temp: {soil_temp} °C, Luminance: {luminance} lux"
+        m2_message = f"CO2: {co2_data} ppm, Moisture: {moisture_data}, Soil Temp: {soil_temp_data} °C, Luminance: {luminance_data} lux"
         aqi_message = f"Date: {date}, AQI: {aqi}"
         
         # 發送消息到 LINE Notify
-        if (co2 != None and moisture != None and soil_temp != None and luminance != None and date != None 
+        if (co2_data != None and moisture_data != None and soil_temp_data != None and luminance_data != None and date != None 
             and aqi != None):
             messages = m2_message + '\n' + aqi_message
             Line.notify(messages)
