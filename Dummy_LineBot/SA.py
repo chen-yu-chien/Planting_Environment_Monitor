992b22265ef9873b87b6ec1d02d5668341c62f26
陳俞蒨 <yuchien.ai13@nycu.edu.tw>
2024-12-24 16:44:14 +0800

    add Linebot dummy device directory

---
+++ Dummy_LineBot/SA.py
@@ -0,0 +1,33 @@
+import random 
+
+ServerURL = 'https://class.iottalk.tw' #For example: 'https://DomainName'
+MQTT_broker = 'iot.iottalk.tw' # MQTT Broker address, for example: 'DomainName' or None = no MQTT support
+MQTT_port = 8883
+MQTT_encryption = True
+MQTT_User = 'iottalk'
+MQTT_PW = 'iottalk2023'
+
+device_model = 'LineBot_process_data'
+# IDF_list = ['Dummy_Sensor']
+ODF_list = ['Dummy_Control']
+device_id = None #if None, device_id = MAC address
+device_name = None
+exec_interval = 1  # IDF/ODF interval
+
+def Dummy_Sensor():
+    return random.randint(0, 100), random.randint(0, 100) 
+
+def Dummy_Control(data:list):
+    print(data[0])
+
+def on_register(r):
+    print(f'Device name: {r["d_name"]}')    
+    '''
+    #You can write some SA routine code here, for example: 
+    import time, DAI
+    while True:
+        DAI.push('Dummy_Sensor', [100, 200])  
+        time.sleep(exec_interval)    
+    '''
+
+
---
+++ Dummy_LineBot/config.py
@@ -0,0 +1,7 @@
+### LineBot Information ###
+ChannelAccessToken = 'zVfbWuvc1uFtUhaDH7pjU0ZjJBiipk7+qqqtZNPlXRZ5hMi59Cc1srUVjKXgHr8rZZh7WbLRg+Y4RMvufFHNKEVTJExf4ba6kjndH/t7jgMDd8Ccd8kKBkTzuKGXi7UVDxygBL+GsWSla5aMMVA8+QdB04t89/1O/w1cDnyilFU='
+ChannelSecret = 'f7a0f1c481c0e8ffa03bb37efd12586e'
+
+### Line ID File Path ###
+idfilePath = './userIdFile'
+
